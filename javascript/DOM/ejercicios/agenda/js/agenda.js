96a2092ef1cb12fca76a52b39467875abc8d6798
Juan Antonio Pérez Jarillo <jantonio.perez@estudioclandestino.es>
2021-01-20 14:30:53 +0100

    dom 3

--- javascript/DOM/ejercicios/agenda/js/agenda.js
+++ javascript/DOM/ejercicios/agenda/js/agenda.js
@@ -1,5 +1,18 @@
-//funcionalidad - logica - acciones
-
 let sectionAgenda = document.querySelector('#agenda');
 
-console.log(sectionAgenda);
+
+const printContact = function (pContactList) {
+    for (let contact of pContactList) {
+        sectionAgenda.innerHTML += `<article>
+            <h2>${contact.name}</h2>
+            <ul>
+                <li>telefono: ${contact.phone}</li>
+                <li>email: ${contact.email}</li>
+            </ul>
+        </article>`;
+    }
+}
+
+printContact(contactos)
+
+//pintar solo los laborales.
--- javascript/DOM/ejercicios/agenda/js/datos.js
+++ javascript/DOM/ejercicios/agenda/js/datos.js
@@ -1,2 +1,27 @@
 //solo los datos
-const contactos = new Array();
+const contactos = new Array(
+    {
+        name: 'Cristian',
+        phone: 9876543,
+        email: 'cristian@gmail.com',
+        category: 'laboral'
+    },
+    {
+        name: 'Mario',
+        phone: 3487654,
+        email: 'mario@gmail.com',
+        category: 'personal'
+    },
+    {
+        name: 'David',
+        phone: 2345678,
+        email: 'david@gmail.com',
+        category: 'personal'
+    },
+    {
+        name: 'Andres',
+        phone: 5678900,
+        email: 'andres@yahoo.com',
+        category: 'laboral'
+    },
+);
